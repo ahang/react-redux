1ea7cad2c1dcaeb1a1cad116b3a39eedf58ec2b6
ahang <hang.andy.t@gmail.com>
2017-06-04 21:45:57 -0500

    more progress on react-redux

--- 01-ReduxSimpleStarter/src/index.js
+++ 01-ReduxSimpleStarter/src/index.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
-const API_KEY = "AIzaSyBDDVZdTSE6chVD2WIjE78sj60ouGz";
+const API_KEY = "AIzaSyCQvWBLh5ax7ivsDhR9XbH1FUEW0uUfi-g";
 
 // Create new Component. Should produce HTML
 // ES6 - const - similar to var; unchangeable variable.
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
+            this.setState({ videos });
+            //this.setState({videos:videos}) -> only when key and prop = same name
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take component and Render it in the DOM
