3d51e4b3b97e0435ad856ebfc92abc3567cc2be0
ahang <hang.andy.t@gmail.com>
2017-06-12 13:27:26 -0500

    learned about using axio (ajax calls) with redux

---
+++ 03-React-Redux-Weather-App/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_KEY = "d0410fc95f70e35454e6e00858f7b8ec";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
