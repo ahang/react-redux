61640a60ca68a07bca8ac386c7c858b01cf83ad4
ahang <hang.andy.t@gmail.com>
2017-06-14 11:54:31 -0500

    completed weather app

--- 03-React-Redux-Weather-App/src/containers/weather_list.js
+++ 03-React-Redux-Weather-App/src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Chart from "../components/chart";
+import GoogleMap from "../components/google_map";
 
 class WeatherList extends Component {
     renderWeather(cityData) {
@@ -8,10 +9,12 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const humidities = cityData.list.map(weather => weather.main.humidity);
         const pressures = cityData.list.map(weather => weather.main.pressure);
+        const { lon, lat } = cityData.city.coord;
+
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td><Chart data={temps} color="orange" units="K" /></td>
                 <td><Chart data={pressures} color="green" units="hPa"/></td>
                 <td><Chart data={humidities} color="black" units="%"/></td>
@@ -27,7 +30,7 @@ class WeatherList extends Component {
                         <th>City</th>
                         <th>Temperature (K)</th>
                         <th>Pressure (hPa)</th>
-                        <th>Humidit (%)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                     {this.props.weather.map(this.renderWeather)}
